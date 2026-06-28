d4c8a48d6ca7f562862214015854b90cd2421551
karan-pandey15 <pandeykaran1515@gmail.com>
2026-06-29 00:26:12 +0530

    st boston edtech

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -94,7 +94,7 @@ export default function Home() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-20">
             <div className="flex flex-col items-start justify-center py-1">
-              <img src="/stbostonlogo.png" alt="St Boston EdTech" className="h-12 md:h-11 w-auto object-contain" />
+              <img src="/stlogo.jpeg" alt="St Boston EdTech" className="h-[4.5rem] md:h-[4.125rem] w-auto object-contain" />
             </div>
 
             {/* Desktop Menu */}
